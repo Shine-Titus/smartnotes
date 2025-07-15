1ff5af73969f5c2e12758a2db83379b797702689
Shine Titus <r.shinetitus@gmail.com>
2025-07-16 01:17:36 +0530

    final static files

--- smartnotes/settings.py
+++ smartnotes/settings.py
@@ -125,6 +125,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'  # 🧠 this is where collectstatic dumps files
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
@@ -136,6 +137,3 @@ LOGIN_REDIRECT_URL = '/smarts/notes'
 
 import os
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'  # this is where collectstatic dumps it
